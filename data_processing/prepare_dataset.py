06aaf644ce4c09329a2ba32ebe6c57f4e7040f91
chavicoski <alvaro.lopez.chilet@gmail.com>
2020-04-01 19:45:47 +0200

    first commit

---
+++ data_processing/prepare_dataset.py
@@ -0,0 +1,92 @@
+import os
+import torch
+from torchvision import transforms
+from PIL import Image
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+# Data paths
+dataset_path = "../dataset"
+train_path = os.path.join(dataset_path, "train")
+train_labels_path = os.path.join(dataset_path, "train_labels.csv")
+
+# Original dataframe
+df = pd.read_csv(train_labels_path)
+# New dataframe to store usefull train data
+new_df = pd.DataFrame(df["ImageId"].unique(), columns=["ImageId"])
+
+########################################
+# DATASET PARTITIONING (train and dev) #
+########################################
+
+# Set the percentaje of samples for the development partition
+dev_partition = 0.2
+
+# Add the partition column and initialize it to 'train'
+new_df["Partition"] = "train"
+# Get number of samples
+n_samples = len(new_df.index)
+# Compute number of samples for development partition
+n_samples_dev = int(n_samples * dev_partition)
+# Change the partition asignation in the dataframe
+new_df["Partition"].iloc[:n_samples_dev] = "dev"
+
+##################################
+# Images and Masks preprocessing #
+##################################
+'''
+Each cell mask is in a separated .png, so we are going to generate the union
+of this masks to avoid making this computation during training.
+
+The masks generated and the input images are going to be preprocessed to store them
+in a pytorch tensor with the size and format ready for training to avoid this computation
+in the input pipeline.
+'''
+# Target size to reshape the images and masks
+target_size=(256, 256)
+
+# Transformations for images -> 1. Resize, 2. Normalize ([0, 1]) and create tensor with color channel
+process_img = transforms.Compose([transforms.Resize(target_size, Image.BICUBIC), transforms.ToTensor()])
+
+# Initialize the columns to store the paths to the preprocessed images and masks
+new_df["ImagePath"] = ""
+new_df["MaskPath"] = ""
+
+# Preprocessing loop
+for index, row in tqdm(new_df.iterrows()):
+    
+    # Get the id of the sample
+    image_id = row["ImageId"]
+    # Open the input image and transform from RGBA to RGB
+    img = Image.open(os.path.join(train_path, image_id, "images", f"{image_id}.png")).convert("RGB")
+    # Apply processing to image and get a pytorch tensor
+    img_tensor = process_img(img)
+
+    # Build the union of the masks
+    mask_tensor = torch.zeros((1,)+target_size)
+    for mask_f in os.listdir(os.path.join(train_path, image_id, "masks")):
+        if mask_f.endswith(".png"):
+            # Load the mask
+            aux_mask = Image.open(os.path.join(train_path, image_id, "masks", mask_f))
+            # Apply processing to mask and get a pytorch tensor
+            aux_mask_tensor = process_img(aux_mask)
+            # Accumulate the mask with the others
+            mask_tensor += aux_mask_tensor
+
+    # Clamp values to between 0 and 1
+    mask_tensor = torch.clamp(mask_tensor, min=0, max=1)
+
+    # Store the image and mask tensors for training
+    img_tensor_path = os.path.join(train_path, image_id, "images", f"{image_id}.pt")
+    mask_tensor_path = os.path.join(train_path, image_id, "masks", f"masks_union.pt")
+    torch.save(img_tensor, img_tensor_path)
+    torch.save(mask_tensor, mask_tensor_path)
+    # Save the tensor's paths in the dataframe
+    row["ImagePath"] = img_tensor_path
+    row["MaskPath"] = mask_tensor_path
+    
+
+# Save the new dataframe
+new_df.to_csv(os.path.join(dataset_path, "data_partition.csv"))
+
---
+++ models/my_models.py
@@ -0,0 +1,118 @@
+import torch
+from torch import nn
+
+#####################
+# Auxiliary modules #
+#####################
+
+class Conv_block(nn.Module):
+    '''
+    Basic convolutional block of the U-net
+        (Conv + BN + ReLU) * 2
+    ''' 
+    def __init__(self, in_channels, out_channels):
+        super(Conv_block, self).__init__()
+        # Define the block architecture
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class Down_block(nn.Module):
+    '''
+    Downsamplig step of U-net
+        max-pool + Conv_block
+    '''
+    def __init__(self, in_channels, out_channels):
+        super(Down_block, self).__init__()
+        # Define the block architecture
+        self.model = nn.Sequential(
+            nn.MaxPool2d(2),
+            Conv_block(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class Up_block(nn.Module):
+    '''
+    Upsampling step of U-net
+        Upsampling_layer + Conv_block
+    '''
+    def __init__(self, in_channels, out_channels):
+        super(Up_block, self).__init__()
+        # Create block layers
+        self.conv_t = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
+        self.conv_block = Conv_block(in_channels, out_channels)
+
+    def forward(self, x, x_skip):
+        x = self.conv_t(x)
+        # Concat upsample output with skip connection before conv_block
+        x_cat = torch.cat([x, x_skip], dim=1)
+        return self.conv_block(x_cat)
+
+
+class Out_block(nn.Module):
+    '''
+    Last convolutional block of the net
+    '''
+    def __init__(self, in_channels, out_channels):
+        super(Out_block, self).__init__()
+        # Create block layers
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+#########
+# U-net #
+#########
+
+class U_net(nn.Module):
+    '''
+    Module that implements the final U-net model
+    '''
+    def __init__(self, in_channels=3, out_channels=1):
+        super(U_net, self).__init__()
+
+        # Define the model layers
+        self.in_block = Conv_block(in_channels, 64)
+        self.down1 = Down_block(64, 128)
+        self.down2 = Down_block(128, 256)
+        self.down3 = Down_block(256, 512)
+        self.down4 = Down_block(512, 1024)
+        self.up1 = Up_block(1024, 512)
+        self.up2 = Up_block(512, 256)
+        self.up3 = Up_block(256, 128)
+        self.up4 = Up_block(128, 64)
+        self.out_block = Out_block(64, out_channels)
+
+    def forward(self, x):
+        x1 = self.in_block(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x6 = self.up1(x5, x4)
+        x7 = self.up2(x6, x3)
+        x8 = self.up3(x7, x2)
+        x9 = self.up4(x8, x1)
+        out = self.out_block(x9)
+        return out
+
+    def get_criterion(self):
+        # This loss combines sigmoid layer with BCELoss for better numerical stability
+        return nn.BCEWithLogitsLoss()
+
+    def get_optimizer(self):
+        #return torch.optim.Adam(self.parameters(), lr=0.001)
+        return torch.optim.SGD(self.parameters(), lr=0.01, momentum=0.9)
